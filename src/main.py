08508f98ba4cbc33c9d3d6dc972253435745a044
Tanmay Deshpande <dsp.tanmay@gmail.com>
2020-08-20 22:40:43 +0530

    added main class

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-import sys
+import pymysql
+from os import get_terminal_size
 
-print(sys.version)
-print(sys.executable)
 
 class Main(object):
-    """Simple class for Case_Study_4"""
-
     def __init__(self):
+        print("-"*get_terminal_size().columns)
+        print("Welcome to the Program!".center(get_terminal_size().columns))
+        print("-"*get_terminal_size().columns)
+        with pymysql.connect("localhost","root","Case_Study_4","tanmay03") as db:
+            self.cursor = db.cursor()
+
+    def run():
         pass
+
+if __name__ == '__main__':
+    app = Main()
+    app.run()
