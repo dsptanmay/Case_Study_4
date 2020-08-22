670e5fee2262db0cdd793028b4e33b618a3e6a7b
Tanmay Deshpande <dsp.tanmay@gmail.com>
2020-08-22 15:10:55 +0530

    al

--- src/main.py
+++ src/main.py
@@ -4,15 +4,16 @@ from os import get_terminal_size
 
 class Main(object):
     def __init__(self):
-        print("-"*get_terminal_size().columns)
+        print("-" * get_terminal_size().columns)
         print("Welcome to the Program!".center(get_terminal_size().columns))
-        print("-"*get_terminal_size().columns)
-        with pymysql.connect("localhost","root","Case_Study_4","tanmay03") as db:
-            self.cursor = db.cursor()
+        print("-" * get_terminal_size().columns)
+        self.db = db = pymysql.connect("localhost", "root", "tanmay03", "Case_Study_4")
+        self.cursor = db.cursor()
 
-    def run():
+    def run(self):
         pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = Main()
     app.run()
